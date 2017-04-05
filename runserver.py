6f6ca4bf0eaa6fd028d2adda1cbd85bf73a16337
JaimeEnergy <Jaime.Martin@EnergyPro.ie>
2017-04-05 12:40:06 +0100

    Proc and runsever update

--- runserver.py
+++ runserver.py
@@ -6,10 +6,7 @@ from os import environ
 from moneen import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    #HOST = '0.0.0.0'
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5555))
+    app.secret_key = os.urandom(24)
+    app.run(host='0.0.0.0', port=port)
